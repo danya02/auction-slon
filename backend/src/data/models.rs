81ef036d5f5b8c25ff9f9ff78cee6e240970c6b0
Grigorii Matiukhin <grigory36m@gmail.com>
2022-12-04 17:10:14 +0300

    Better SQL enums

--- backend/src/data/models.rs
+++ backend/src/data/models.rs
@@ -1,51 +1,38 @@
-use diesel::backend::Backend;
 use diesel::deserialize::{self, FromSql};
-use diesel::serialize::{self, Output, ToSql};
+use diesel::serialize::{self, IsNull, Output, ToSql};
+use diesel::sqlite::Sqlite;
 use diesel::{prelude::*, AsExpression};
 use diesel::{sql_types::*, FromSqlRow};
+use enum_primitive::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, AsExpression, FromSqlRow)]
-#[repr(i32)]
-#[diesel(sql_type = Integer)]
-pub enum UserRole {
-    Buyer = 0,
-    Seller = 1,
-}
-
-impl UserRole {
-    pub fn value(&self) -> i32 {
-        match self {
-            UserRole::Buyer => 0,
-            UserRole::Seller => 1,
-        }
+enum_from_primitive! {
+    #[derive(Debug, Clone, Copy, PartialEq, AsExpression, FromSqlRow)]
+    #[repr(i32)]
+    #[diesel(sql_type = Integer)]
+    pub enum UserRole {
+        Buyer = 0,
+        Seller = 1,
     }
 }
 
-impl<DB> ToSql<Integer, DB> for UserRole
+impl ToSql<Integer, Sqlite> for UserRole
 where
-    DB: Backend,
-    i32: ToSql<Integer, DB>,
+    i32: ToSql<Integer, Sqlite>,
 {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, DB>) -> serialize::Result {
-        match self {
-            UserRole::Buyer => 0.to_sql(out),
-            UserRole::Seller => 1.to_sql(out),
-        }
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Sqlite>) -> serialize::Result {
+        out.set_value(*self as i32);
+        Ok(IsNull::No)
     }
 }
 
-impl<DB> FromSql<Integer, DB> for UserRole
+impl FromSql<Integer, Sqlite> for UserRole
 where
-    DB: Backend,
-    i32: FromSql<Integer, DB>,
+    i32: FromSql<Integer, Sqlite>,
 {
-    fn from_sql(bytes: diesel::backend::RawValue<'_, DB>) -> deserialize::Result<Self> {
-        // Note: this is kinda code duplication
-        match i32::from_sql(bytes)? {
-            0 => Ok(UserRole::Buyer),
-            1 => Ok(UserRole::Seller),
-            x => Err(format!("Unrecognized variant {} of enum UserRole", x).into()),
-        }
+    fn from_sql(bytes: diesel::backend::RawValue<'_, Sqlite>) -> deserialize::Result<Self> {
+        let val = i32::from_sql(bytes)?;
+        UserRole::from_i32(val)
+            .ok_or(format!("Unrecognized variant {} of enum UserRole", val).into())
     }
 }
 
