b86bc3475cf3bb459fea1d5e0746586ae05aeda1
Danya Generalov <danya@danya02.ru>
2023-07-30 10:59:52 +0300

    Translating admin UI captions.

--- frontend/admin/src/admin_ui.rs
+++ frontend/admin/src/admin_ui.rs
@@ -41,16 +41,16 @@ pub fn AdminUserInterface() -> Html {
     let content = match &ctx.auction_state {
         AuctionState::WaitingForAuction => html! {
             <VerticalStack>
-                <h1>{"Auction is not yet started"}</h1>
+                <h1>{"Аукцион еще не начат"}</h1>
                 <setup::SetupAuction/>
-                <button class="btn btn-success" onclick={start_auction_cb}>{"Begin auction"}</button>
+                <button class="btn btn-success" onclick={start_auction_cb}>{"Начать аукцион"}</button>
             </VerticalStack>
         },
         AuctionState::AuctionOver(report) => html! {
             <VerticalStack>
-                <h1>{"Auction has now been concluded"}</h1>
+                <h1>{"Аукцион завершен"}</h1>
                 <AuctionReportView report={report.clone()} />
-                <button class="btn btn-success" onclick={start_auction_anew_cb}>{"Return to start of auction"}</button>
+                <button class="btn btn-success" onclick={start_auction_anew_cb}>{"Вернуться в начало аукциона"}</button>
             </VerticalStack>
         },
 
@@ -65,23 +65,23 @@ pub fn AdminUserInterface() -> Html {
             html! {
                 <HorizontalStack>
                     <VerticalStack>
-                        <h1>{"Please choose an item to auction off next"}</h1>
+                        <h1>{"Выбери предмет для продажи"}</h1>
                         <ChooseItemToSell />
-                        <button class="btn btn-danger" onclick={conclude_cb}>{"Conclude auction"}</button>
+                        <button class="btn btn-danger" onclick={conclude_cb}>{"Завершить аукцион"}</button>
                     </VerticalStack>
                     <VerticalStack>
-                        <h1>{"Transfer money manually"}</h1>
+                        <h1>{"Передать деньги вручную"}</h1>
                         {
                             if ctx.admin_state.holding_account_balance == 0 {
                                 html!(
                                     <div class="alert alert-success">
-                                        {"Holding account balance: "}<MoneyDisplay money={0} />
+                                        {"Временный счет: "}<MoneyDisplay money={0} />
                                     </div>
                                 )
                             } else {
                                 html!(
                                     <div class="alert alert-warning">
-                                        {"Holding account balance: "}<MoneyDisplay money={ctx.admin_state.holding_account_balance} />
+                                        {"Временный счет: "}<MoneyDisplay money={ctx.admin_state.holding_account_balance} />
                                     </div>
                                 )
                             }
